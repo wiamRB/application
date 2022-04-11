4a29a79e7399a0ef72dfffa23b0e1546aa134d8d
wiamRB <101800097+wiamRB@users.noreply.github.com>
2022-04-11 17:01:31 +0000

    Update formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -14,14 +14,3 @@
 					 </form>
 		</body>
 </html>
-<?php
- $link1 = mysqli_connect("192.168.56.102","wiam","Wiam@2000","app") or die
-       ("Error " . mysqli_error($link1));
-if (isset ($_POST['enre'])){
-$titre=$_POST['titre'];
-$prix=$_POST['prix'];
-$sql2 = "INSERT INTO books VALUES(NULL,'$titre','$prix')";
-//echo $sql2;
-mysqli_query ($link1,$sql2);
-} 
-?>
